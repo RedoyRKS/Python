d368617238742b96e22f1af209b81676a7196303
redoyRKS <rshaha201355@bscse.uiu.ac.bd>
2023-01-22 21:11:07 +0600

    Beginners _level

---
+++ hello.py
@@ -0,0 +1,27 @@
+# print("Hello Redoy")
+# name = 'UIU'
+# id_uiu = 10.5
+# print(id_uiu)
+# print(name)
+
+# name = input("What is your name? ")
+# color = input("What is your favorit_color? ")
+# print(name + ' like ' + color)
+
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2023 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# Weight_in_pounds = input('Weight_pounds: ')
+# Convert_weight_kg = int(Weight_in_pounds) * 0.45
+# print(Convert_weight_kg)
+
+# course = '''
+# Hi Redoy,
+# Here is our first email to you.
+# Thank you,
+# The support team.
+# '''
+# print(course)
